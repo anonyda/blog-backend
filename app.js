8f564502cd4d71e51e8686edff7e8469da94a4df
Nida <zaynida123@gmail.com>
2021-08-25 16:35:38 +0530

    basic setup

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Successful.'
+    })
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({path: "./config.env"});
+
+mongoose.connect(process.env.DB_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    useCreateIndex: true,
+    useFindAndModify: false
+    })
+    .then((connection) => {
+        console.log('Connected to DB');
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening to requests at port ${process.env.PORT}`);
+        })
+        
+    })
+    .catch((error) => {
+        console.log('Error in DB Connection');
+});
+        
